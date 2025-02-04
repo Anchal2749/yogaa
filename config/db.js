86368038ab3152f84b3ef494ac91be0cfd265d84
Anchal2749 <anchalmaurya474@gmail.com>
2025-02-04 23:33:34 +0530

    changes in db

--- config/db.js
+++ config/db.js
@@ -1,17 +1,20 @@
+
+
+
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-//     console.log("MongoDB Connected!");
-//   } catch (error) {
-//     console.error("MongoDB Connection Error:", error);
-//     process.exit(1);
-//   }
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected!"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected!");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
